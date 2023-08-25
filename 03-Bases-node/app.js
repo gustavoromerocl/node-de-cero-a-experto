7c62a725b0370bbb97b4196066243a8cdec50800
romerodevelopments <gandresrp@gmail.com>
2023-08-24 23:21:15 -0400

    Bases de Node - Requerir paquetes

--- 03-Bases-node/app.js
+++ 03-Bases-node/app.js
@@ -1,11 +1,22 @@
+const fs = require('fs')
+
 console.clear();
 console.log("==================================")
 console.log("Tabla del 5")
 console.log("==================================")
 
 
-const base = 5;
+const base = 5
+let salida = ''
 
 for (let index = 1; index <= 10; index++) {
-  console.log(`${base} x ${index} =`, 5 * index)
+  salida += `${base} x ${index} = ${base * index}\n`
 }
+
+console.log(salida)
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+  if (err) throw error;
+  
+  console.log(`tabla-${base}.txt creado`)
+})
