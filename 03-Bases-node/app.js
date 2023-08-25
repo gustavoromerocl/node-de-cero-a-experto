84cd124f336092457af4239b6d2feb00a0293607
romerodevelopments <gandresrp@gmail.com>
2023-08-24 23:05:39 -0400

    Bases de node - Inicio del proyecto

---
+++ 03-Bases-node/app.js
@@ -0,0 +1,11 @@
+console.clear();
+console.log("==================================")
+console.log("Tabla del 5")
+console.log("==================================")
+
+
+const base = 5;
+
+for (let index = 1; index <= 10; index++) {
+  console.log(`${base} x ${index} =`, 5 * index)
+}
