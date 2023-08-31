0ec353bce9ad4b1ec03a541a6fc54656087f2ced
romerodevelopments <gandresrp@gmail.com>
2023-08-31 19:22:22 -0400

    Bases de Node - Configuracion de yargs independiente

--- 03-Bases-node/app.js
+++ 03-Bases-node/app.js
@@ -1,20 +1,5 @@
 const { crearArchivo } = require('./helpers/multiplicar')
-const argv = require('yargs')
-  .option('b', {
-    alias: 'base',
-    type: 'number',
-    demandOption: true
-  })
-  .option('l', {
-    alias: 'list',
-    type: 'boolean',
-    default: false
-  })
-  .check((argv, options) => {
-    if (isNaN(argv.b)) throw 'La base debe ser un número'
-    return true
-  })
-  .argv
+const argv = require('./config/yargs')
 console.clear();
 
 
