8782898c687b940fc346931ab2d8eb76b1962d5f
romerodevelopments <gandresrp@gmail.com>
2023-09-02 12:55:13 -0400

    Aplicacion de consola interactiva - stdin - stdout - Readline

--- 04-Tareas-app/app.js
+++ 04-Tareas-app/app.js
@@ -1,8 +1,13 @@
 require('colors')
+
+const { mostrarMenu, pausa } = require('./helpers/mensajes')
+
 console.clear()
 
 const main = async () => {
   console.log('Hola mundo')
+  mostrarMenu()
+  // pausa()
 }
 
 main()
