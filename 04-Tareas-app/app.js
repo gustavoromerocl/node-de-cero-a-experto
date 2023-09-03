085ea8be254ff15a8b0ffb482609fd4b6e4c2a66
romerodevelopments <gandresrp@gmail.com>
2023-09-03 16:48:06 -0300

    Aplicacion de consola interactiva - Crear y listar tareas

--- 04-Tareas-app/app.js
+++ 04-Tareas-app/app.js
@@ -1,13 +1,30 @@
 require('colors')
-const { inquirerMenu, pausa } = require('./helpers/inquirer')
+const { 
+  inquirerMenu, 
+  pausa,
+  leerInput 
+} = require('./helpers/inquirer')
 const Tarea = require('./models/tarea')
+const Tareas = require('./models/tareas')
 
 const main = async () => {
   console.log('Hola mundo')
   let opt = ''
+  const tareas = new Tareas()
+
   do {
     opt = await inquirerMenu()
-    console.log({ opt })
+
+    switch (opt) {
+      case 1:
+        const desc = await leerInput('Descripción:')
+        tareas.crearTarea(desc)
+        console.log(desc)
+        break;
+      case 2:
+        console.log(tareas._listado)
+        break;
+    }
     if (opt !== 0) await pausa()
   } while (opt !== 0);
 }
--- 04-Tareas-app/helpers/inquirer.js
+++ 04-Tareas-app/helpers/inquirer.js
@@ -61,7 +61,28 @@ const pausa = async () => {
   console.log('\n')
 }
 
+const leerInput = async ( message ) => {
+  const question = [
+    {
+      type: 'input',
+      name: 'desc',
+      message,
+      validate( value ) {
+        if(value.length === 0) {
+          return 'Por favor ingrese un valor'
+        }
+        return true
+      }
+    }
+  ]
+
+  const { desc } = await inquirer.prompt(question)
+
+  return desc
+}
+
 module.exports = {
   inquirerMenu,
-  pausa
+  pausa,
+  leerInput
 }
