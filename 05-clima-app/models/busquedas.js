aa66b056951ce9f099d67f132de9e80c9860e93e
romerodevelopments <gandresrp@gmail.com>
2023-09-11 20:37:51 -0300

    Aplicacion de clima - Realizar peticiones http desde node

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 
 class Busquedas {
 
@@ -8,7 +9,14 @@ class Busquedas {
   }
 
   async ciudad(lugar = '') {
-
+    // console.log('lugar', lugar)
+    try {
+      const { data } = await axios.get('https://reqres.in/api/users?page=2')
+      console.log('data', data)
+      return []
+    } catch (error) {
+      return []
+    }
   }
 }
 
