68c774cf42c17591b2fc560a46d27cf7106427fe
romerodevelopments <gandresrp@gmail.com>
2023-09-11 21:19:00 -0300

    Aplicacion de clima - Mapbox search api y token de acceso

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -11,7 +11,7 @@ class Busquedas {
   async ciudad(lugar = '') {
     // console.log('lugar', lugar)
     try {
-      const { data } = await axios.get('https://reqres.in/api/users?page=2')
+      const { data } = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madrid.json?types=country%2Cregion&language=es&access_token=pk.eyJ1IjoiZ3VzdGF2b3JvbWVyb2NsIiwiYSI6ImNsbWZqYzgzaDAzcjkzdGxtcjk5czhtdXkifQ.d6bSXr7YAr31grp9n9UWhw')
       console.log('data', data)
       return []
     } catch (error) {
