1aa9ab202352f209d21b9969c40520aff7d41591
romerodevelopments <gandresrp@gmail.com>
2023-09-12 21:45:42 -0300

    Aplicacion de clima - Crear instancias de axios

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -8,10 +8,23 @@ class Busquedas {
     
   }
 
+  get paramsMapBox() {
+    return {
+      'language': 'es',
+      'access_token': 'pk.eyJ1IjoiZ3VzdGF2b3JvbWVyb2NsIiwiYSI6ImNsbWZqYzgzaDAzcjkzdGxtcjk5czhtdXkifQ.d6bSXr7YAr31grp9n9UWhw',
+      'limit': 5
+    }
+  }
+
   async ciudad(lugar = '') {
-    // console.log('lugar', lugar)
+    console.log('lugar', lugar)
     try {
-      const { data } = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madrid.json?types=country%2Cregion&language=es&access_token=pk.eyJ1IjoiZ3VzdGF2b3JvbWVyb2NsIiwiYSI6ImNsbWZqYzgzaDAzcjkzdGxtcjk5czhtdXkifQ.d6bSXr7YAr31grp9n9UWhw')
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+        params: this.paramsMapBox
+      })
+      const { data } = await instance.get()
+      // const { data } = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madrid.json?types=country%2Cregion&language=es&access_token=pk.eyJ1IjoiZ3VzdGF2b3JvbWVyb2NsIiwiYSI6ImNsbWZqYzgzaDAzcjkzdGxtcjk5czhtdXkifQ.d6bSXr7YAr31grp9n9UWhw')
       console.log('data', data)
       return []
     } catch (error) {
