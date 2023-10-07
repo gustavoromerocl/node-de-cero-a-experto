b54b30fc7dd663b48d4746f2eb886baf4f596afb
romerodevelopments <gandresrp@gmail.com>
2023-10-07 08:40:35 -0300

    Alcances del REST server - Conexion a la base de datos con Mongoose

--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { dbConnection } = require('../database/config')
 
 
 class Server {
@@ -7,11 +8,19 @@ class Server {
     this.app = express()
     this.port = process.env.PORT
     this.usersRoutePath = '/api/users'
+    
+    //Conectar a base de datos
+    this.dbConnect()
+
     //Middlewares
     this.middlewares()
 
     //routes
     this.routes()
+  }
+
+  async dbConnect() {
+    await dbConnection()
   }
 
   middlewares() {
