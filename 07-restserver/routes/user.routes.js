682b1ccf9a11fec3de146c46048afb511e8a8523
romerodevelopments <gandresrp@gmail.com>
2023-10-08 22:54:30 -0300

    Alcances del REST server - Validar todos los campos necesarios

--- 07-restserver/routes/user.routes.js
+++ 07-restserver/routes/user.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express')
-const { 
-  usuariosGet, 
-  usuariosPut, 
-  usuariosPost, 
-  usuariosDelete 
+const {
+  usuariosGet,
+  usuariosPut,
+  usuariosPost,
+  usuariosDelete
 } = require('../controllers/usuarios.controller')
 const { check } = require('express-validator')
+const { validarCampos } = require('../middlewares/validation-pipe')
 
 const router = Router()
 
@@ -14,8 +15,12 @@ router.get('/', usuariosGet)
 router.put('/:id', usuariosPut)
 
 router.post('/', [
+  check('fullName', 'The name is required').not().isEmpty(),
+  check('password', 'The password is more than 6 characters').isLength({ min: 6 }),
   check('mail', 'The email isnt valid').isEmail(),
-] ,usuariosPost)
+  check('role', 'The role isnt valid').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+  validarCampos
+], usuariosPost)
 
 router.delete('/', usuariosDelete)
 
