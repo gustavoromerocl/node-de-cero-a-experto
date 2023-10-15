928fb92f019af963d186372c408615864800b622
romerodevelopments <gandresrp@gmail.com>
2023-10-15 10:56:21 -0300

    Alcances del REST server - GET: retornar numero total de registros

--- 07-restserver/controllers/usuarios.controller.js
+++ 07-restserver/controllers/usuarios.controller.js
@@ -6,10 +6,14 @@ const { validationResult } = require('express-validator')
 const usuariosGet = async (req = request, res = response) => {
   // const { apiKey = 'no api key' } = req.params
   const { limit = 5, skip = 0 } = req.query;
-  const usuarios = await User.find()
+  const query = { state: true }
+  const [ users, count ] = await Promise.all([
+    User.find(query)
     .limit(+limit)
-    .skip(+skip)
-  res.json({ usuarios })
+    .skip(+skip),
+    User.countDocuments(query)
+  ]) 
+  res.json({ users, count })
 }
 
 const usuariosPut = async (req, res = response) => {
