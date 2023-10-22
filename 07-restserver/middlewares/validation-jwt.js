45062274818734322c540ac427b95491d7cc0874
romerodevelopments <gandresrp@gmail.com>
2023-10-22 15:06:05 -0300

    Autenticacion de usuario JWT - obtener la informacion del usuario autenticado

--- 07-restserver/middlewares/validation-jwt.js
+++ 07-restserver/middlewares/validation-jwt.js
@@ -1,7 +1,8 @@
 const { request, response } = require('express')
 const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
-const ValidateJWT = (req = request, res = response, next) => {
+const ValidateJWT = async (req = request, res = response, next) => {
   const token = req.header('x-token')
   console.log(token)
   if (!token) {
@@ -10,7 +11,20 @@ const ValidateJWT = (req = request, res = response, next) => {
 
   try {
     const { uid } = jwt.verify(token, process.env.JWT_SECRET_KEY)
-    req.uid = uid;
+    const user = await User.findById( uid )
+
+    if(!user) {
+      return res.status(401).json({
+        msg: 'request doesnt have a valid token - user doesnt exist'
+      })
+    }
+
+    if(!user.state) {
+      return res.status(401).json({
+        msg: 'request doesnt have a valid token - state false'
+      })
+    }
+    req.user = user
     next()
   } catch (error) {
     console.log(error)
