819c30b59ef40117f4eaf7c909e027ccd09cd6e7
romerodevelopments <gandresrp@gmail.com>
2023-10-22 11:12:13 -0300

    Autenticacion de usuario JWT - login de usuario

--- 07-restserver/controllers/auth.controller.js
+++ 07-restserver/controllers/auth.controller.js
@@ -1,10 +1,41 @@
 const { response } = require("express");
+const User = require("../models/user");
+const bcryptjs = require('bcryptjs')
 
-const login = (req, res = response) => {
+const login = async (req, res = response) => {
+  const { mail, password } = req.body;
+  try {
+    const user = await User.findOne({ mail })
+
+    if(!user) {
+      return res.status(400).json({
+        msg: 'User or passsword isnt valid'
+      })
+    }
+
+    if(!user.state) {
+      return res.status(400).json({
+        msg: 'User or passsword isnt valid'
+      })
+    }
+
+    const validPassword = bcryptjs.compareSync( password, user.password );
+
+    if(!validPassword) {
+      return res.status(400).json({
+        msg: 'User or passsword isnt valid - password'
+      })
+    }
 
     res.json({
-        msg: 'ok',
+      msg: 'ok',
     })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      msg: 'some was wrong'
+    })
+  }
 }
 
 module.exports = { login }
