a728e34d2d4b912af89f54619a6d0bcc74fa2d9c
romerodevelopments <gandresrp@gmail.com>
2023-11-04 20:18:36 -0300

    categorias y productos - crear una categoria

--- 07-restserver/models/category.js
+++ 07-restserver/models/category.js
@@ -1,28 +1,28 @@
 const { Schema, model } = require('mongoose');
 
 const CategorySchema = Schema({
-    nombre: {
-        type: String,
-        required: [true, 'El nombre es obligatorio'],
-        unique: true
-    },
-    estado: {
-        type: Boolean,
-        default: true,
-        required: true
-    },
-    usuario: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    }
+  name: {
+    type: String,
+    required: [true, 'the name is required'],
+    unique: true
+  },
+  state: {
+    type: Boolean,
+    default: true,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 });
 
 
-CategorySchema.methods.toJSON = function() {
-    const { __v, estado, ...data  } = this.toObject();
-    return data;
+CategorySchema.methods.toJSON = function () {
+  const { __v, estado, ...data } = this.toObject();
+  return data;
 }
 
 
-module.exports = model( 'Category', CategorySchema );
+module.exports = model('Category', CategorySchema);
--- 07-restserver/routes/categories.routes.js
+++ 07-restserver/routes/categories.routes.js
@@ -1,12 +1,23 @@
 const { Router } = require('express')
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validation-pipe');
+const { ValidateJWT } = require('../middlewares');
+const { createCategory } = require('../controllers/categories.controller');
+
 
 const router = Router()
 
 router.get('/', (req, res) => res.json({ msg: 'get' }))
+
 router.get('/:id', (req, res) => res.json({ msg: 'get id' }))
-router.post('/', (req, res) => res.json({ msg: 'post' }))
+
+//Cualquier persona con token - privado
+router.post('/', [
+  ValidateJWT,
+  check('name', 'the name is required').not().isEmpty(),
+  validarCampos
+], createCategory)
+
 router.put('/:id', (req, res) => res.json({ msg: 'update id' }))
 router.delete('/:id', (req, res) => res.json({ msg: 'delete id' }))
 
