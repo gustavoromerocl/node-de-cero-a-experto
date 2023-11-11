764b9a48c359aacc33a098435d641292d6fe83b0
romerodevelopments <gandresrp@gmail.com>
2023-11-11 20:51:32 -0300

    carga de archivos - validar la extension

--- 07-restserver/controllers/uploads.controller.js
+++ 07-restserver/controllers/uploads.controller.js
@@ -3,21 +3,30 @@ const path = require('path')
 
 const loadFiles = (req, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
-    res.status(400).json({ msg: 'No files were uploaded.' });
+    res.status(400).json({ msg: 'No files were uploaded.' })
     return;
   }
+ 
+  console.log('req.files >>>', req.files) // eslint-disable-line
+  const { file } = req.files
+  const splitName = file.name.split('.') 
+  const fileExtension = splitName[splitName.length - 1]
+  const validExtensions = ['png', 'jpg', 'jpeg', 'gif']
   
-  console.log('req.files >>>', req.files); // eslint-disable-line
-  const { file } = req.files;
+  if(!validExtensions.includes(fileExtension)) {
+    return res.status(400).json({
+      msg: `The file extension ${fileExtension} isnt valid`
+    })
+  }
 
-  uploadPath = path.join(__dirname, '../uploads/' + file.name);
+  uploadPath = path.join(__dirname, '../uploads/' + file.name)
 
   file.mv(uploadPath, function (err) {
     if (err) {
-      return res.status(500).json({err});
+      return res.status(500).json({err})
     }
 
-    res.send('File uploaded to ' + uploadPath);
+    res.send('File uploaded to ' + uploadPath)
   });
 }
 
