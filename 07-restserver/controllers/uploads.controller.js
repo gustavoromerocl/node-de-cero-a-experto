31b5562fc251d4c672c383deab2b58ed2232c731
romerodevelopments <gandresrp@gmail.com>
2023-11-11 21:00:07 -0300

    carga de archivos - ubicar y cambiar nombre

--- 07-restserver/controllers/uploads.controller.js
+++ 07-restserver/controllers/uploads.controller.js
@@ -1,4 +1,5 @@
 const { response } = require("express")
+const { v4: uuidv4 } = require('uuid')
 const path = require('path')
 
 const loadFiles = (req, res = response) => {
@@ -6,24 +7,26 @@ const loadFiles = (req, res = response) => {
     res.status(400).json({ msg: 'No files were uploaded.' })
     return;
   }
- 
+
   console.log('req.files >>>', req.files) // eslint-disable-line
   const { file } = req.files
-  const splitName = file.name.split('.') 
+  const splitName = file.name.split('.')
   const fileExtension = splitName[splitName.length - 1]
   const validExtensions = ['png', 'jpg', 'jpeg', 'gif']
-  
-  if(!validExtensions.includes(fileExtension)) {
+
+  if (!validExtensions.includes(fileExtension)) {
     return res.status(400).json({
       msg: `The file extension ${fileExtension} isnt valid`
     })
   }
 
-  uploadPath = path.join(__dirname, '../uploads/' + file.name)
+  const tempName = uuidv4() + '.' + fileExtension
+
+  uploadPath = path.join(__dirname, '../uploads/' + tempName)
 
   file.mv(uploadPath, function (err) {
     if (err) {
-      return res.status(500).json({err})
+      return res.status(500).json({ err })
     }
 
     res.send('File uploaded to ' + uploadPath)
