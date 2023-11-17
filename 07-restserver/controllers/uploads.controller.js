caaebaa49fae049dca9dd0c42ae836f3ada44101
romerodevelopments <gandresrp@gmail.com>
2023-11-16 22:25:17 -0300

    categorias y productos - borrar archivos del servidor

--- 07-restserver/controllers/uploads.controller.js
+++ 07-restserver/controllers/uploads.controller.js
@@ -1,6 +1,8 @@
+const fs = require('fs')
+const path = require('path')
 const { response } = require("express");
 const { uploadFile } = require("../helpers");
-const { User } = require("../models");
+const { User, Product } = require("../models");
 
 const loadFiles = async (req, res = response) => {
   console.log('req.files >>>', req.files) // eslint-disable-line
@@ -25,18 +27,32 @@ const updateImage = async (req, res = response) => {
   switch (collection) {
     case 'users':
       model = await User.findById(id);
-      if(!model) {
+      if (!model) {
         return res.status(400).json({
           msg: 'The user isnt exists'
         })
       }
       break;
-  
+
+    case 'products':
+      model = await Product.findById(id);
+      if (!model) {
+        return res.status(400).json({
+          msg: 'The product isnt exists'
+        })
+      }
+      break;
+
     default:
-      res.status(400).json({msg: 'The collection isnt valid'})
+      res.status(400).json({ msg: 'The collection isnt valid' })
       break;
   }
-
+  if (model.img) {
+    const pathImg = path.join(__dirname, '../uploads', collection, model.img)
+    if (fs.existsSync(pathImg)) {
+      fs.unlinkSync(pathImg)
+    }
+  }
   const filename = await uploadFile(req.files, undefined, collection);
   model.img = filename
   await model.save()
