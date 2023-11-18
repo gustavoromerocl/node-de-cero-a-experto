5a8e031bffe7d1e958a69100accf42d8bd70d73a
romerodevelopments <gandresrp@gmail.com>
2023-11-17 22:49:15 -0300

    categorias y productos - carga de imagenes a cloudinary

--- 07-restserver/controllers/uploads.controller.js
+++ 07-restserver/controllers/uploads.controller.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
 const path = require('path')
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config(process.env.CLOUDINARY_URL);
+
 const { response } = require("express");
 const { uploadFile } = require("../helpers");
 const { User, Product } = require("../models");
@@ -59,6 +63,49 @@ const updateImage = async (req, res = response) => {
   res.json(model)
 }
 
+const updateImageCloudinary = async (req, res = response) => {
+  const { id, collection } = req.params;
+
+  let model;
+
+  switch (collection) {
+    case 'users':
+      model = await User.findById(id);
+      if (!model) {
+        return res.status(400).json({
+          msg: 'The user isnt exists'
+        })
+      }
+      break;
+
+    case 'products':
+      model = await Product.findById(id);
+      if (!model) {
+        return res.status(400).json({
+          msg: 'The product isnt exists'
+        })
+      }
+      break;
+
+    default:
+      res.status(400).json({ msg: 'The collection isnt valid' })
+      break;
+  }
+  if (model.img) {
+    //TO DO DELETE
+  }
+  try {
+    const { tempFilePath } = req.files.file
+    const { secure_url } = await cloudinary.uploader.upload(tempFilePath)
+    model.img = secure_url
+    await model.save()
+    res.json(model)
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
 const showImage = async (req, res = response) => {
   const { id, collection } = req.params;
 
@@ -101,5 +148,6 @@ const showImage = async (req, res = response) => {
 module.exports = {
   loadFiles,
   updateImage,
-  showImage
+  showImage,
+  updateImageCloudinary
 }
--- 07-restserver/routes/uploads.routes.js
+++ 07-restserver/routes/uploads.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { loadFiles, updateImage, showImage } = require('../controllers/uploads.controller')
+const { loadFiles, updateImage, showImage, updateImageCloudinary } = require('../controllers/uploads.controller')
 const { check } = require('express-validator')
 const { validarCampos } = require('../middlewares')
 const { isValidCollection, isValidUploadFile } = require('../helpers')
@@ -13,7 +13,9 @@ router.put('/:collection/:id', [
   check('id', 'The id isnt MongoId').isMongoId(),
   check('collection').custom( c => isValidCollection(c, ['users', 'products'])),
   validarCampos
-], updateImage)
+], updateImageCloudinary)
+// ], updateImage)
+
 
 router.get('/:collection/:id', [
   check('id', 'The id isnt MongoId').isMongoId(),
