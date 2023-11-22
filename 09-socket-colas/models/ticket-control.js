1a8cd22f8d19e071711d7ac3508571374e6ab85a
romerodevelopments <gandresrp@gmail.com>
2023-11-22 08:20:07 -0300

    sockets: aplicacion de cola - modelo: siguiente y atender nuevo ticket

--- 09-socket-colas/models/ticket-control.js
+++ 09-socket-colas/models/ticket-control.js
@@ -1,6 +1,13 @@
 const path = require('path')
 const fs = require('fs')
 
+class Ticket {
+  constructor(number, desktop) {
+    this.number = number,
+    this.desktop = desktop
+  }
+}
+
 class TicketControl {
   constructor() {
     this.last     = 0;
@@ -33,7 +40,36 @@ class TicketControl {
 
   saveDb() {
     const dbPath = path.join(__dirname, '../db/data.json')
-    fs.writeFileSync(dbPath, this.toJson)
+    fs.writeFileSync(dbPath, JSON.stringify(this.toJson))
+  }
+
+  next() {
+    this.last += 1
+    const ticket = new Ticket(this.last, null)
+    this.tickets.push(ticket)
+
+    this.saveDb()
+    return 'Ticket' + ticket.number
+  }
+
+  attendTicket() {
+    if( this.tickets.length === 0 ) {
+      return null
+    }
+
+    const ticket = this.tickets.shift()
+    
+    ticket.desktop = desktop
+
+    this.lastFour.unshift(ticket)
+
+    if(this.lastFour.length > 4) {
+      this.lastFour.splice(-1, 1)
+    }
+
+    this.saveDb()
+
+    return ticket
   }
 }
 
