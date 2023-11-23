1b901586c993a50b774b2b7aeb433499d7b0807b
romerodevelopments <gandresrp@gmail.com>
2023-11-23 08:32:17 -0300

    sockets: aplicacion de cola - preparar pantalla de ecritorio

--- 09-socket-colas/public/js/escritorio.js
+++ 09-socket-colas/public/js/escritorio.js
@@ -1,3 +1,35 @@
+//HTML references
+const lbldesktop = document.querySelector('h1')
+const btnNew = document.querySelector('button')
+const searchParams = new URLSearchParams()
+
+if(!searchParams.has('escritorio')) {
+  window.location = 'index.html'
+  throw new Error('El escritorio es obligatorio')
+}
+
+const desktop = searchParams.get('escritorio')
+lbldesktop.innerText = desktop
+const socket = io();
 
 
-console.log('Escritorio HTML');
+
+socket.on('connect', () => {
+  // console.log('Conectado');
+  btnNew.disabled = false
+});
+
+socket.on('disconnect', () => {
+  // console.log('Desconectado del servidor');
+  btnNew.disabled = true
+});
+
+socket.on('last-ticket', (last) => {
+  // lblNewTicket.innerText = 'Ticket ' + last
+})
+
+btnNew.addEventListener('click', () => {
+  // socket.emit('next-ticket', null, (ticket) => {
+  //   lblNewTicket.innerText = ticket
+  // });
+});
