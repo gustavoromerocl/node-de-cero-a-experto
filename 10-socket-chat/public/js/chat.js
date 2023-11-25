d6224bab082db5d4b78b5845a71ba33d3cdb0aaf
romerodevelopments <gandresrp@gmail.com>
2023-11-25 18:09:11 -0300

    sockets con autenticacion - validar token frontend

--- 10-socket-chat/public/js/chat.js
+++ 10-socket-chat/public/js/chat.js
@@ -1,2 +1,28 @@
-const socket = io()
+let usuario = null
+let socket = null
+
+const validarJWT = async () => {
+  const token = localStorage.getItem('token') || ''
+
+  if (token.length < 10) {
+    window.location = 'index.html'
+    throw new Error('No hay token en el server')
+  }
+
+  const resp = await fetch(url, {
+    headers: { 'x-token': token }
+  })
+
+  const { usuario: userDB, token: tokenDB} = await resp.json()
+  localStorage.setItem('token', tokenDB)
+  usuario = userDB
+}
+
+const main = async () => {
+  await validarJWT()
+}
+
+main()
+
+// const socket = io()
 
